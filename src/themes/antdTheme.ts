0e2179675f88c617483da32346489f20d34e3117
Rajeev Singh <rsinghh68@gmail.com>
2024-02-21 04:15:54 +0530

    Added dark theme rather than using tailwind for dark theme

--- src/themes/antdTheme.ts
+++ src/themes/antdTheme.ts
@@ -2,8 +2,23 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import { theme } from 'antd';
 import { ThemeConfig } from 'antd/es/config-provider/context';
 
 export const antdTheme: ThemeConfig = {
 	hashed: false
+};
+
+export const darkTheme: ThemeConfig = {
+	algorithm : theme.darkAlgorithm,
+	components: {
+		Tabs: {
+			inkBarColor: '#34C06E',
+			itemHoverColor: '#34C06E',
+			itemSelectedColor: '#34C06E'
+		},
+		Typography : {
+			fontFamily : 'Barlow '
+		}
+	}
 };
