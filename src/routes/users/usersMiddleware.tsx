89b3fe4ff58a95df05769e93a80b2f1dbaa10fa4
Rajeev Singh <rsinghh68@gmail.com>
2024-02-21 04:12:01 +0530

    modified routes for users and added profiles.tsc

--- src/routes/users/usersMiddleware.tsx
+++ src/routes/users/usersMiddleware.tsx
@@ -3,15 +3,15 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Users from 'src/Screens/Users/index';
 import ProfileView from 'src/Screens/Users/profileInfo';
-import Show from 'src/Screens/Users/show';
 
 const UserMiddleware = () => {
 	return (
 
 		<Routes>
 			<Route path="/profile/:id" element={<ProfileView />} />
-			<Route path='/' element={<Show />} />
+			<Route path='/' element={<Users />} />
 		</Routes>
 
 	);
